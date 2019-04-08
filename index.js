d49d52aecb79556b1c9bbc89af65ba8ad5e86f1a
Talent Antonio <antonio.talenty@gmail.com>
2019-04-08 15:27:34 +0100

    api/login end point

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// Manage cohorts (id, name)
+// Manage users (id, name, password)
 const express = require('express');
 const helmet = require('helmet');
-const knex = require('knex');
 const cors = require('cors');
 const bcrypt = require('bcrypt')
 
@@ -19,7 +18,7 @@ server.use(cors());
  server.get('/', (req, res) => {
     res.send("It's alive!");
   });
-
+//  post api register
   server.post('/api/register', (req, res) => {
     let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10)
@@ -32,6 +31,24 @@ server.use(cors());
         res.status(500).json(error);
       });
   });
+//   post api login
+server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+    const hash = bcrypt.hashSync(user.password, 10)
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
 // 
 const port = process.env.PORT || 2500;
 server.listen(port, () =>
