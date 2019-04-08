d6c79c2f6557b58f3ba8b6f46eae1b01875a903f
Talent Antonio <antonio.talenty@gmail.com>
2019-04-08 15:14:56 +0100

    get and post user endpoints

--- index.js
+++ index.js
@@ -16,8 +16,22 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
  //
+ server.get('/', (req, res) => {
+    res.send("It's alive!");
+  });
 
-
+  server.post('/api/register', (req, res) => {
+    let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 // 
 const port = process.env.PORT || 2500;
 server.listen(port, () =>
