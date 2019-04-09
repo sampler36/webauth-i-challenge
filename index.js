dab92a94634cfc6f985b7bb26471fb902ebde03d
Talent Antonio <antonio.talenty@gmail.com>
2019-04-09 15:44:33 +0100

    sessions and cookies

--- index.js
+++ index.js
@@ -3,22 +3,49 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcrypt')
+const session = require('express-session')
+const knexSessionStore = require('connect-session-knex')(session)
+
 
 // const knexConfig = require('./knexfile').development;
 // const db = knex(knexConfig);
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model');
+const authRouter = require('./auth/auth-router');
 
 const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+const sessionConfig = {
+  name: 'monkey doo',
+ secret: 'we like banana',
+ cookie: {
+   maxAge: 1000 * 60 * 60,
+   secure: false,
+ },
+ httpOnly: true, 
+ resave: false,
+ saveUninitialized: false,
+ // storing in the data base
+ store: new knexSessionStore({
+   knex:require('./database/dbConfig'),
+   tablename: 'sessions',
+   sidfieldname: 'sid',
+   createtable: true,
+   clearInterval: 1000 * 60 * 60
+ })
+}
+
+server.use(session(sessionConfig));
+server.use('/api/auth', authRouter);
+
  //
  server.get('/', (req, res) => {
     res.send("It's alive!");
   });
-//  post api register
+//  post api register Users
   server.post('/api/register', (req, res) => {
     let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10)
@@ -49,26 +76,14 @@ server.post('/api/login', (req, res) => {
   });
 
 //    api users
-// function restricted(req, res, next){
-//     let { username,password } = req.headers;
-      
-//     if(username && password){
-//     Users.findBy({username})
-//     .first()
-//     .then(user => {
-//       if (user && bcrypt.compareSync(password, user.password)){
-//         next()
-//       } else{
-//         res.status(401).json({message: "invalid cred "})
-//       }
-//     })
-//     .catch(error => {
-//       res.status(501).json({mesage:"unexpected error"})
-//     })
-//   }  else {
-//     res.status(400).json({message: "no cred available"})
-//    }
-//   }
+function restricted(req, res, next){
+    let { username,password } = req.headers;
+    if(req.session && req.session.user) {
+      next();
+    } else {
+      res.status(401).json({message: 'Not allowed'})
+    }
+  }
 server.get('/api/users', (req, res) => {
     Users.find()
       .then(users => {
